dc716d231fdd4346ed03e9f825db95a65bfecf97
shaoxiong1233 <91181903+shaoxiong1233@users.noreply.github.com>
2021-12-11 10:08:24 +0800

    Revert "删除Test_new"
    This reverts commit e1f16859962e69ff4be9664650e0051ebf90c7f5.

---
+++ Test_new/src/main/java/first/kafka/Flink_protobuf.java
@@ -0,0 +1,14 @@
+package first.kafka;
+
+public class Flink_protobuf {
+/*    public static void main(String[] args) {
+        final ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+
+// register the class of the serializer as serializer for a type
+        env.getConfig().registerTypeWithKryoSerializer(TestProto.class, MyCustomSerializer.class);
+
+// register an instance as serializer for a type
+        MySerializer mySerializer = new MySerializer();
+        env.getConfig().registerTypeWithKryoSerializer(MyCustomType.class, mySerializer);
+    }*/
+}
